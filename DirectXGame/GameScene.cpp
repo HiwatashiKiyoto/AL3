35bac19424c6ee8628b8833950458e6f4fba2ef2
hiwatashi <k025g0031@g.neec.ac.jp>
2026-02-09 17:51:49 +0900

    ライザップ

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,18 +1,45 @@
 #include "GameScene.h"
+#include"player.h"
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() 
+GameScene::~GameScene() 
 {
+	delete model_;
+	delete debugCamera_;
+	delete player_;
+}
 
+void GameScene::Initialize()
+{ 
+	textureHandle_ = TextureManager::Load("dvd.png"); 
+
+	model_ = Model::Create();
+
+	//デバッグカメラの生成
+	debugCamera_ = new DebugCamera(1280,720);
+
+	debugCamera_->GetCamera(); 
+
+	camera_ = 
+
+	//自キャラの生成
+	player_ = new Player();
+
+	//自キャラの初期化
+	player_->Initialize(model_,textureHandle_, );
 }
 
 void GameScene::Updata() 
-{
+{ 
+	debugCamera_->Update();
 
+	//自キャラの更新
+	player_->Update();
 }
 
 void GameScene::Draw() 
 {
-
+	//自キャラの描画
+	player_->Draw();
 }
